3b3b0f8d74548e50f5c4f9f5f385ca29243574e3
MRFIJ <zqq1111@yandex.ru>
2020-12-28 16:34:54 +0400

    start server creation

--- src/main/java/com/taskmanager/server/Server.java
+++ src/main/java/com/taskmanager/server/Server.java
@@ -7,25 +7,47 @@ import java.util.Scanner;
 public class Server {
     public static void main(String[] args) throws IOException {
 
-        try (ServerSocket serverSocket = new ServerSocket(9000);
-             Socket socket = serverSocket.accept();
-             Scanner scanner = new Scanner(socket.getInputStream())) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(9000);
+            while (true) {
 
-            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
-
-            while (scanner.hasNextLine()) {
-                String string = scanner.nextLine();
-                if (string.equals("sing up")) {
-                    writer.println("up");
-                }
-                if (string.equals("sing in")) {
-                    writer.println("in");
-                }
-                if (string.equals("exit")) {
-                    break;
-                }
+                Socket socket = serverSocket.accept();
+                new ThreadServer(socket);
 
             }
+        } catch (IOException e) {
+
+        }
+
+    }
+}
+
+class ThreadServer extends Thread {
+    Socket socket;
+
+    public ThreadServer(Socket socket) {
+
+        this.socket = socket;
+        start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                BufferedReader read = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                BufferedWriter writ = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                String str = read.readLine();
+                if (str.equals("END"))
+                    break;
+                System.out.println("Получено: " + str);
+                System.out.println(str);
+            }
+
+
+        } catch (
+                IOException e) {
+            e.printStackTrace();
         }
 
     }
